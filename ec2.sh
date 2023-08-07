4988580a2b2fb355ade105ab7ab8a5fe31bb0caf
Dirisala999 <abhilaash.dirisala@gmail.com>
2023-08-07 18:37:34 -0400

    update

--- ec2.sh
+++ ec2.sh
@@ -2,3 +2,6 @@ aws ec2 run-instances --image-id ami-0f34c5ae932e6f0e4 --count 1 --instance-type
 
 
 aws ec2 describe-instances --filters "Name=instance-type,Values=t2.micro" --query "Reservations[].Instances[].InstanceId"
+
+
+aws ec2 terminate-instances --instance-ids i-0cf0800b488f56047
