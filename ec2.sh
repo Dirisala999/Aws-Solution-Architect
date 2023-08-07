e6ce1b29211554055d5d9e3bdad055a63ba69296
Dirisala999 <abhilaash.dirisala@gmail.com>
2023-08-07 18:32:54 -0400

    update

--- ec2.sh
+++ ec2.sh
@@ -1,1 +1,4 @@
-aws ec2 run-instances --image-id ami-0f34c5ae932e6f0e4 --count 1 --instance-type t2.micro --key-name Server_kp --security-group-ids sg-0136d83bacc4f850f --subnet-id subnet-05402e9cd08c39b02
+aws ec2 run-instances --image-id ami-0f34c5ae932e6f0e4 --count 1 --instance-type t2.micro --key-name Server_kp --security-group-ids sg-0136d83bacc4f850f --subnet-id subnet-05402e9cd08c39b02 
+
+
+aws ec2 describe-instances --filters "Name=instance-type,Values=t2.micro" --query "Reservations[].Instances[].InstanceId"
